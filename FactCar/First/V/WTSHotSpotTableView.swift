58c9454b38f368525aed8c314aa8cf3872fb16e9
邢浩 <xxxinghao@sina.com>
2017-08-09 10:55:09 +0800

    创建要闻工程模板
    创建要闻工程模板

---
+++ FactCar/First/V/WTSHotSpotTableView.swift
@@ -0,0 +1,14 @@
+//
+//  WTSHotSpotTableView.swift
+//  FactCar
+//
+//  Created by LeoTai on 2017/8/9.
+//  Copyright © 2017年 邢浩. All rights reserved.
+//
+
+import UIKit
+
+class WTSHotSpotTableView: WTSRootTableView {
+  
+
+}
---
+++ FactCar/First/V/WTSRootTableView.swift
@@ -0,0 +1,23 @@
+//
+//  WTSRootTableView.swift
+//  FactCar
+//
+//  Created by LeoTai on 2017/8/9.
+//  Copyright © 2017年 邢浩. All rights reserved.
+//
+
+import UIKit
+
+class WTSRootTableView: UITableView {
+
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    print("......")
+  }
+  
+  override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    super.touchesBegan(touches, with: event)
+    print("触摸了")
+  }
+  
+}
